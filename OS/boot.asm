db9a23513461fe2c91fa0d430280a7af88c48589
AuracleTech <VirgilFabre@outlook.com>
2022-08-30 13:25:58 -0400

    GDT & Protected mode (32 bit)

--- OS/boot.asm
+++ OS/boot.asm
@@ -1,39 +1,52 @@
 [org 0x7c00]
-mov [BOOT_DISK], dl ; get and set the boot disk value
+mov [BOOT_DISK], dl
 
-; setting up the stack
+cli ; clear interrupts flags and disable interrupts
+lgdt [GDT_descriptor] ; load GDT
+mov eax, cr0 ;
+or eax, 1    ; enable paging by setting bit 0 of CR0 to 1
+mov cr0, eax ;
+jmp CODE_SEG:start_protected_mode
 
-xor ax, ax                          
-mov es, ax
-mov ds, ax
-mov bp, 0x8000
-mov sp, bp
+GDT_start: ; must be at the end of real mode code
+    GDT_null: ; at the beginning of GDT there must be a null descriptor
+        dd 0x0 ; the size of the descriptor must be 00000000
+        dd 0x0 ; the descriptor is null so ends with 00000000
 
-mov bx, 0x7e00 ; Where to read the data
+    GDT_code: ; descriptor for code segment
+        dw 0xffff ; limit
+        dw 0x0 ; base address
+        db 0x0 ; base present
+        db 0b10011010 ; present, privilege, type = 1001 type flags = 1010
+        db 0b11001111 ; other flags = 1100, granularity = 1111
+        db 0x0 ; the last 8 bits are reserved and must be zero
 
-; reading the disk
+    GDT_data: ; descriptor for data segment
+        dw 0xffff ; limit
+		dw 0x0 ; base address
+		db 0x0 ; base present
+		db 0b10010010 ; present, privilege, type = 0001 type flags
+		db 0b11001111 ; other flags = 1100, granularity = 1111
+		db 0x0 ; the last 8 bits are reserved and must be zero
 
-mov ah, 2 ; set the ah register to match with the 02 function code (write to STDOUT)
-mov al, 1 ; Number of sectors to read
-mov ch, 0 ; Cylinder (starts at 0)
-mov cl, 2 ; Sector number (starts at 1)
-mov dh, 0 ; Head number (starts at 0)
-mov dl, [BOOT_DISK] ; Drive number (starts at 0)
-int 0x13 ; BIOS read disk
+GDT_end:
+	; placeholder
 
-; printing what is in the next sector
+GDT_descriptor:
+    dw GDT_end - GDT_start - 1 ; size of GDT
+    dd GDT_start ; address of GDT's start
 
-mov ah, 0x0e
-mov al, [0x7e00] ; Located data is at 0x7c00 + 0x200 = 0x7e00
-int 0x10
-jmp $
+CODE_SEG equ GDT_code - GDT_start
+DATA_SEG equ GDT_data - GDT_start
+
+[bits 32]
+start_protected_mode:
+    mov al, 'A' ; print A
+    mov ah, 0xe0 ; black foreground light yellow background
+    mov [0xb8000], ax ; set to video memory location
+    jmp $
+
 BOOT_DISK: db 0
 
-; magic padding
-
-times 510-($-$$) db 0 ; file size padding to 510 bytes
-dw 0xaa55 ; signature to make sure the file is valid
-
-; register A at 0x7e00 (at 512 bytes, right after the boot sector)
-
-times 512 db 'A'
+times 510-($-$$) db 0
+dw 0xaa55
